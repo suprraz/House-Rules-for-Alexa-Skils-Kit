62ea999e277d4115319ae0cdc3978c9a24fbf12e
Razvan Suprovici <rsuprovici@gmail.com>
2016-05-26 22:47:23 -0700

    Submission 2

--- intent_schema.js
+++ intent_schema.js
@@ -27,7 +27,49 @@
     {
       "intent": "DeleteHouseRuleNumber",
       "slots": [
-        {
+        {{
+          "intents": [
+            {
+              "intent": "SetHouseRule",
+              "slots": [
+                {
+                  "name": "HouseRule",
+                  "type": "AMAZON.LITERAL"
+                }
+              ]
+            },
+            {
+              "intent": "ListHouseRules"
+            },
+            {
+              "intent": "DeleteAllHouseRules"
+            },
+            {
+              "intent": "DeleteHouseRuleAbout",
+              "slots": [
+                {
+                  "name": "RuleContent",
+                  "type": "AMAZON.LITERAL"
+                }
+              ]
+            },
+            {
+              "intent": "DeleteHouseRuleNumber",
+              "slots": [
+                {
+                  "name": "RuleNumber",
+                  "type": "AMAZON.NUMBER"
+                }
+              ]
+            },
+            {
+              "intent": "AMAZON.HelpIntent"
+            },
+            {
+              "intent": "AMAZON.StopIntent"
+            }
+          ]
+        }
           "name": "RuleNumber",
           "type": "AMAZON.NUMBER"
         }
